97d55b37721960907abfe58193704e44e5eed66e
Constant <c.zwijnenberg@gmail.com>
2019-01-24 16:07:10 +0100

    first commit, episode 4

---
+++ resources/views/about.blade.php
@@ -0,0 +1,11 @@
+@extends('layout')
+
+@section('content')
+
+    <h1>About us</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quisquam est, a ullam dignissimos delectus ipsum corporis quasi exercitationem
+     incidunt suscipit explicabo aliquam amet perferendis labore sint illum quaerat quam magnam. Lorem ipsum dolor sit, amet consectetur
+      adipisicing elit. Accusamus nam quisquam laboriosam quis ratione, explicabo laborum vero, cupiditate officia dolore voluptatem provident!
+       Dolores eius fugit, ratione possimus blanditiis quis non?
+    </p>
+@endsection
